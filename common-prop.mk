dda253434d0faf2062c6de5c4187b80393d9f63b
Pavel Dubrova <pashadubrova@gmail.com>
2020-12-29 13:29:25 +0200

    common-prop: Set ro.enable_boot_charger_mode to true
    Allow healthd to boot system directly from charger mode
    rather than initiating a reboot [1].
    
    [1] https://android.googlesource.com/platform/system/core/+/e4b7b294f37d9b64d6b7c1931e2c9bfb1a500d68
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- common-prop.mk
+++ common-prop.mk
@@ -315,3 +315,7 @@ endif
 # Reduce cost of scrypt for FBE CE decryption
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.scrypt_params=15:3:1
+
+# Allows healthd to boot directly from charger mode rather than initiating a reboot
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.enable_boot_charger_mode=1
